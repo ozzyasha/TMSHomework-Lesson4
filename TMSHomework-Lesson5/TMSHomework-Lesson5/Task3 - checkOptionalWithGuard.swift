c677219a15c8133a7fa4cb54de90de76c8ca575b
Natallia Mazur <natashamazur@bk.ru>
2023-12-01 20:12:29 +0300

    Add 3rd task

--- TMSHomework-Lesson5/TMSHomework-Lesson5/Task3 - checkOptionalWithGuard.swift
+++ TMSHomework-Lesson5/TMSHomework-Lesson5/Task3 - checkOptionalWithGuard.swift
@@ -9,6 +9,32 @@ import Foundation
 
 // TODO: 3.Написать функцию, использующую оператор guard для проверки опционального значения. Если значение равно nil, выведите сообщение об ошибке, в противном случае выполните операцию с извлеченным значением. 3-4 примера
 
-func checkOptionalWithGuard (guardOptional: Int?){
+func checkOptionalWithGuard (guardOptional: Int?) -> String {
+    guard guardOptional != nil else {
+        return "guardOptional is empty"
+    }
     
+    return("\(String(describing: guardOptional)) value has an Optional type and \(guardOptional!) value has an Int type")
+}
+
+func findSumOfDoubleWithGuard (firstOptionalDouble: Double?, secondOptionalDouble: Double?) -> String {
+    guard let firstDoubleValue = firstOptionalDouble else {
+        return "firstOptionalValue is empty"
+    }
+    
+    guard let secondDoubleValue = secondOptionalDouble else {
+        return"secondOptionalValue is empty"
+    }
+    
+    let sumOfValues = firstDoubleValue + secondDoubleValue
+    return("The sum of double values is \(sumOfValues)")
+}
+
+func convertOptionalDoubleToIntWithGuard (doubleOptional: Double?) -> String {
+    guard doubleOptional != nil else {
+        return "Can't convert doubleOptional to Int. doubleOptional is empty"
+    }
+    
+    let intValueOfDoubleOptional = Int(doubleOptional!)
+    return("The Int value of \(String(describing: doubleOptional)) is \(intValueOfDoubleOptional)")
 }
--- TMSHomework-Lesson5/TMSHomework-Lesson5/ViewController.swift
+++ TMSHomework-Lesson5/TMSHomework-Lesson5/ViewController.swift
@@ -9,6 +9,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    let divider = "------------------------------------" // разделитель для вывода в консоли
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -19,19 +21,40 @@ class ViewController: UIViewController {
         let optionalNil = makeOptionalDoubled(optional: nil)
         print(optionalNil)
         
+        
+        print(divider)
         // MARK: вызов функции #2
         
         unwrapOptional(wrappedOptional: 15)
         unwrapOptional(wrappedOptional: nil)
         
+        print(divider)
         // MARK: вызов функции #3
         
+        print(checkOptionalWithGuard(guardOptional: 10))
+        print(checkOptionalWithGuard(guardOptional: nil))
+        
+        print(divider)
+        
+        print(findSumOfDoubleWithGuard(firstOptionalDouble: 12.0, secondOptionalDouble: 7.5))
+        print(findSumOfDoubleWithGuard(firstOptionalDouble: nil, secondOptionalDouble: 7.5))
+        print(findSumOfDoubleWithGuard(firstOptionalDouble: 12.0, secondOptionalDouble: nil))
+        
+        print(divider)
+        
+        print(convertOptionalDoubleToIntWithGuard(doubleOptional: 12.0))
+        print(convertOptionalDoubleToIntWithGuard(doubleOptional: nil))
+        
+        print(divider)
         // MARK: вызов функции #4
         
+        print(divider)
         // MARK: вызов функции #5
         
+        print(divider)
         // MARK: вызов функции #6
         
+        print(divider)
     }
 
 
