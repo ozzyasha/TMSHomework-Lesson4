1ea69c916a2abde377f5d06ad6f8c359ef840723
Natallia Mazur <natashamazur@bk.ru>
2023-12-01 17:43:24 +0300

    Add files for all tasks

---
+++ TMSHomework-Lesson5/TMSHomework-Lesson5/Task6 - showClothesMessage.swift
@@ -0,0 +1,10 @@
+//
+//  Task6 - showClothesMessage.swift
+//  TMSHomework-Lesson5
+//
+//  Created by Наталья Мазур on 1.12.23.
+//
+
+import Foundation
+
+// TODO: 6.Создайте перечисление Clothes. Напишите функцию, которая принимает Clothes в качестве параметра и выводит сообщение о выбранной одежде.
