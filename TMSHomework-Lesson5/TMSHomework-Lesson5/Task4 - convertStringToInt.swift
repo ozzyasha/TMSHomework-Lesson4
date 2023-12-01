23014ea57a91105ff713756825284c9b7587650b
Natallia Mazur <natashamazur@bk.ru>
2023-12-01 20:12:29 +0300

    Add 3rd task

--- TMSHomework-Lesson5/TMSHomework-Lesson5/Task4 - convertStringToInt.swift
+++ TMSHomework-Lesson5/TMSHomework-Lesson5/Task4 - convertStringToInt.swift
@@ -9,6 +9,6 @@ import Foundation
 
 // TODO: 4.Напишите функцию, которая принимает строку и пытается преобразовать ее в целое число. Если преобразование возможно, верните число, если нет - nil
 
-func convertStringToInt (string: String?) -> Int? {
-    
-}
+//func convertStringToInt (string: String?) -> Int? {
+//    
+//}
