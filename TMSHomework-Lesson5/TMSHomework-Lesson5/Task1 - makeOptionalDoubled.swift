8c61fc8c5cf59c314d2aea3649195c5b521cbf2a
Natallia Mazur <natashamazur@bk.ru>
2023-12-01 17:16:46 +0300

    Add 2nd task and relocate 1st task in separate file

---
+++ TMSHomework-Lesson5/TMSHomework-Lesson5/Task1 - makeOptionalDoubled.swift
@@ -0,0 +1,19 @@
+//
+//  Task1 - makeOptionalDoubled.swift
+//  TMSHomework-Lesson5
+//
+//  Created by Наталья Мазур on 1.12.23.
+//
+
+import Foundation
+
+// MARK: 1.Написать функцию, которая принимает опциональное число и, если оно есть, умножает его на 2
+
+func makeOptionalDoubled (optional: Int?) -> String{
+    if optional != nil {
+        let doubledOptional = "Результат умножения опционального числа: \(optional! * 2)"
+        return doubledOptional
+    } else {
+        return "Значения нет"
+    }
+}
